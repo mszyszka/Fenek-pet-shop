364c2eed8113d195dfd61e1a7d0bc1f6909c0ff7
Michal <mszyszka223@gmail.com>
2019-03-30 18:00:42 +0100

    Work in progress

--- js/script.js
+++ js/script.js
@@ -1,9 +1,19 @@
+//Im writing this code using jQuery and elements of ES6
+
+//fire owlCarousel
+$(document).ready(function(){
+    $(".owl-carousel").owlCarousel();
+});
+
 //operating on nav
-$('.hamburger').on('click', function(){
+$('.hamburger').on('click', ()=> {
+
     const menu = $('.menu');
     const mainNav = $('.main-nav');
 
     $('.hamburger').toggleClass('change')
     menu.toggleClass('open');
     mainNav.toggleClass('white-background');
-});
+
+    }
+);
