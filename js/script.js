8dbb0a6a75f8d0873451c32c3fd527e12607b80a
Michal <mszyszka223@gmail.com>
2019-04-11 11:41:54 +0200

    I wrote removeNavStyleOver600px()

--- js/script.js
+++ js/script.js
@@ -1,14 +1,32 @@
 //Im writing this code using jQuery and elements of ES6
 
-
+//Im creating navObject so I can use its properties in any place in code
+let navObjet = {
+    menu : $('.menu'),
+    hamburger : $('.hamburger')
+}
 //operating on nav
-$('.hamburger').on('click', ()=> {
-
-    const menu = $('.menu');
-    const hamburger = $('.hamburger');
-
-    hamburger.toggleClass('change white-background')
-    menu.toggleClass('open white-background');
-
+navObjet.hamburger.on('click', ()=> {
+    navObjet.hamburger.toggleClass('change white-background')
+    navObjet.menu.toggleClass('open white-background');
     }
 );
+
+function removeNavStyleOver600px(x) {
+    if (x.matches) { // If media query matches
+        //width < 600px
+        return;
+    } else {
+        //width > 600px
+        let hamburgerHasClass = navObjet.hamburger.hasClass('change white-background');
+        let menuHasClass = navObjet.menu.hasClass('open white-background');
+        if (hamburgerHasClass && menuHasClass) {
+            navObjet.hamburger.removeClass('change white-background');
+            navObjet.menu.removeClass('open white-background');
+        } else { return; }
+    }
+  }
+  
+  var x = window.matchMedia("(max-width: 600px)")
+  removeNavStyleOver600px(x) // Call listener function at run time
+  x.addListener(removeNavStyleOver600px) // Attach listener function on state changes
